5bd84a28da5ca5147ae89415fa17840cd5ebb40b
SankhaYapa <sankhapriyamantha55@gmail.com>
2022-09-25 12:52:49 +0530

    update

---
+++ lib/conrollers/auth_controller.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:video_conferencing/utils/utils.dart';
+
+class AuthController {
+  //Firebase Auth instance created
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<bool> signInWithGoogle(BuildContext context) async {
+    bool res = false;
+    try {
+      GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleSignInAuthentication =
+          await googleUser?.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication?.accessToken,
+        idToken: googleSignInAuthentication?.idToken,
+      );
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+
+      User? user = userCredential.user;
+
+      if (user != null) {
+        if (userCredential.additionalUserInfo!.isNewUser) {
+          await _firestore.collection('users').doc(user.uid).set({
+            'username': user.displayName,
+            'uid': user.uid,
+            'profilePhoto': user.photoURL
+          });
+        }
+        res = true;
+      }
+    } on FirebaseAuthException catch (e) {
+      showSnackBar(context, e.message!);
+      res = false;
+    }
+    return res;
+  }
+}
